f7f5e07f4c86d505f8fe70795a5c685ec74e8436
alliehkw <allie@hkw.io>
2023-11-06 10:51:59 -0500

    added dif nav colors for homepage v not home page. now need to change them based on scroll position as well

--- parts/sticky-nav-offcanvas-topbar.php
+++ parts/sticky-nav-offcanvas-topbar.php
@@ -6,12 +6,13 @@
  */
 ?>
 
-<div data-sticky-container>
+<!-- // Apply homepage-class to nav bar if on homepage to allow for proper color formatting  -->
+<div data-sticky-container class="<?php echo (is_home() || is_front_page()) ? 'homepage-class' : ''; ?>">
 	<div class="top-bar" id="top-bar-menu" data-sticky data-sticky-on="small" data-options="marginTop:0;" style="width:100%">
 		<div class="header-container">
 			<div class="top-bar-left float-left">
 				<ul class="menu">
-					<li class="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg ?>"/></a></li>
+					<li class="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg ?>"/></a></li>
 				</ul>
 			</div>
 			<div class="top-bar-right">
