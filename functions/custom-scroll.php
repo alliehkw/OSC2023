696b1a885b10172f7da0781a80c01b1227eb7046
alliehkw <allie@hkw.io>
2023-11-06 13:59:31 -0500

    nav bar formatting updated for everything except category pages the logo is the wrong color

--- functions/custom-scroll.php
+++ functions/custom-scroll.php
@@ -5,6 +5,8 @@ add_action('wp_footer', 'add_custom_scroll_script');
 function add_custom_scroll_script() {
 ?>
 <script type="text/javascript">
+    var isBlogPage = <?php echo json_encode(is_home()); ?>;
+    var isCategoryPage = <?php echo json_encode(is_category()); ?>;
     document.addEventListener('DOMContentLoaded', function() {
         window.addEventListener('scroll', function() {
             var header = document.querySelector('.sticky-container');
@@ -17,6 +19,9 @@ function add_custom_scroll_script() {
                 header.classList.remove('scrolled');
                 header.classList.add('unscrolled');
                 imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg ?>";
+            }
+            if (isBlogPage || isCategoryPage) {
+                imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg ?>";
             }
         });
     });
--- parts/sticky-nav-offcanvas-topbar.php
+++ parts/sticky-nav-offcanvas-topbar.php
@@ -7,13 +7,23 @@
 ?>
 
 <!-- // Apply homepage-class to nav bar if on homepage to allow for proper color formatting  -->
-<div data-sticky-container class="<?php echo (is_home() || is_front_page()) ? 'homepage-class' : ''; ?>">
+<div data-sticky-container class="<?php echo (is_front_page()) ? 'homepage-class' : ''; ?><?php echo (is_home() || is_category()) ? ' blogpage-class' : ''; ?>">
 	<div class="top-bar" id="top-bar-menu" data-sticky data-sticky-on="small" data-options="marginTop:0;" style="width:100%">
 		<div class="header-container">
 			<div class="inner-header">
 				<div class="top-bar-left float-left">
 					<ul class="menu">
-						<li class="logo"><a href="<?php echo home_url(); ?>"><img class="image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg ?>"/></a></li>
+						<li class="logo">
+							<a href="<?php echo home_url(); ?>">
+							<?php if ( is_home() ): ?>
+								<!-- If is_home() is true, use a different image -->
+								<img class="image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg" alt="Logo"/>
+							<?php else: ?>
+								<!-- If is_home() is false, use the default image -->
+								<img class="image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg" alt="Logo"/>
+							<?php endif; ?>
+							</a>
+						</li>
 					</ul>
 				</div>
 				<div class="top-bar-right">
