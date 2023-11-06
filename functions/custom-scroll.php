9cce5c10cc5547dffbc12cbf02571d3ad047872b
alliehkw <allie@hkw.io>
2023-11-06 15:23:09 -0500

    fixed side nav formatting and added in formatting for category nav bars

--- functions/custom-scroll.php
+++ functions/custom-scroll.php
@@ -7,22 +7,28 @@ function add_custom_scroll_script() {
 <script type="text/javascript">
     var isBlogPage = <?php echo json_encode(is_home()); ?>;
     var isCategoryPage = <?php echo json_encode(is_category()); ?>;
+    var isSinglePage = <?php echo json_encode(is_single()); ?>;
     document.addEventListener('DOMContentLoaded', function() {
         window.addEventListener('scroll', function() {
             var header = document.querySelector('.sticky-container');
             var imageLogo = document.querySelector('.image-logo');
+            if (isBlogPage || isCategoryPage || isSinglePage) {
+                imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg ?>";
+            }
             if (window.scrollY > 70) {
                 header.classList.add('scrolled');
                 header.classList.remove('unscrolled');
-                imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg ?>";
+                if (!isBlogPage && !isCategoryPage && !isSinglePage) {
+                    imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg ?>";
+                }
             } else {
                 header.classList.remove('scrolled');
                 header.classList.add('unscrolled');
-                imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg ?>";
+                if (!isBlogPage && !isCategoryPage && !isSinglePage) {
+                    imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg ?>";
+                }
             }
-            if (isBlogPage || isCategoryPage) {
-                imageLogo.src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg ?>";
-            }
+
         });
     });
 </script>
--- parts/sticky-nav-offcanvas-topbar.php
+++ parts/sticky-nav-offcanvas-topbar.php
@@ -7,7 +7,7 @@
 ?>
 
 <!-- // Apply homepage-class to nav bar if on homepage to allow for proper color formatting  -->
-<div data-sticky-container class="<?php echo (is_front_page()) ? 'homepage-class' : ''; ?><?php echo (is_home() || is_category()) ? ' blogpage-class' : ''; ?>">
+<div data-sticky-container class="<?php echo (is_front_page()) ? 'homepage-class' : ''; ?><?php echo (is_home() || is_category() || is_single()) ? ' blogpage-class' : ''; ?>">
 	<div class="top-bar" id="top-bar-menu" data-sticky data-sticky-on="small" data-options="marginTop:0;" style="width:100%">
 		<div class="header-container">
 			<div class="inner-header">
@@ -18,11 +18,11 @@
 							<?php if ( is_home() ): ?>
 								<!-- If is_home() is true, use a different image -->
 								<img class="image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg" alt="Logo"/>
-							<?php elseif ( is_category() ): ?>
-								<!-- If is_category() is true, use a different image -->
+							<?php elseif ( is_category() || is_single() ): ?>
+								<!-- If is_category() or is_single() is true, use a different image -->
 								<img class="image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-1.svg" alt="Logo"/>
 							<?php else: ?>
-								<!-- If is_home() is false, use the default image -->
+								<!-- Default -->
 								<img class="image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo-colored-2.svg" alt="Logo"/>
 							<?php endif; ?>
 							</a>
