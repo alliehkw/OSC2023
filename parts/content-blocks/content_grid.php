22754c419cd9c8f4b5d3a58cd5d81f3062704379
alliehkw <allie@hkw.io>
2023-11-08 07:27:28 -0500

    added in past research projects

--- parts/content-blocks/content_grid.php
+++ parts/content-blocks/content_grid.php
@@ -313,6 +313,7 @@ if (get_sub_field('custom_padding')) {
                                     $author = get_sub_field('author');
                                     $summary = get_sub_field('summary');
                                     $externalLinkPresent = get_sub_field('include_external_link');
+                                    $mediaPresent = get_sub_field('include_media_with_post');
                                     $linkClass = $externalLinkPresent == 1 ? "active-link" : "not-a-link";
                                     $linkName = "#";
                                     if ($externalLinkPresent == 1) : 
@@ -335,6 +336,37 @@ if (get_sub_field('custom_padding')) {
                                                     echo $summary;
                                                 echo '</div>';
                                             echo '</a>';
+                                            if ($mediaPresent && have_rows('media')):
+                                                echo '<div class="medias">';
+                                                while (have_rows('media')): the_row();
+                                                    $cover_image = get_sub_field('cover_image');
+                                                    $media_upload = get_sub_field('media');
+                                                        $image_url = $cover_image['url'];
+                                                        $media_item_title = $cover_image['title'];
+                                                        if ($media_upload) :
+                                                            echo '<a href="' . $media_upload['url'] . '" target="_blank" class="media-item-with-link">'; 
+                                                                echo '<div class="media-item">';
+                                                                    echo '<div class="media-item-image">';
+                                                                        echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($media_item_title) . '">';
+                                                                    echo '</div>';
+                                                                    echo '<div class="media-title">';
+                                                                        echo esc_html($media_item_title);
+                                                                    echo '</div>';
+                                                                echo '</div>';
+                                                            echo '</a>';
+                                                        else : 
+                                                            echo '<div class="media-item">';
+                                                                echo '<div class="media-item-image">';
+                                                                    echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($media_item_title) . '">';
+                                                                echo '</div>';
+                                                                echo '<div class="media-title">';
+                                                                    echo esc_html($media_item_title);
+                                                                echo '</div>';
+                                                            echo '</div>';
+                                                        endif;
+                                                endwhile;
+                                                echo '</div>';
+                                            endif;
                                         echo '</div>';
                                 endwhile;
                             endif;
