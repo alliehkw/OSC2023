86bc98b03fae2a8c0bfdce62672648bf6243880a
alliehkw <allie@hkw.io>
2023-11-08 07:15:15 -0500

    fixed current and ongoing research. now working on past research

--- parts/content-blocks/content_grid.php
+++ parts/content-blocks/content_grid.php
@@ -262,44 +262,47 @@ if (get_sub_field('custom_padding')) {
                                     $mediaPresent = get_sub_field('include_media_with_post');
                                     $externalLinkPresent = get_sub_field('include_external_link');
                                     echo '<ul class="cell xlarge-12 research-info">';
-                                    echo '<li class="research-title">';
-                                        if ($externalLinkPresent == 1) :
-                                            $externalLink = get_sub_field('external_link');
-                                            echo '<a href="' . $externalLink . '" class="external-link" target="_blank">';
+                                        echo '<li class="research-title">';
+                                            if ($externalLinkPresent == 1) :
+                                                $externalLink = get_sub_field('external_link');
+                                                echo '<a href="' . $externalLink . '" class="external-link" target="_blank">';
+                                                    echo $title;
+                                                echo '</a>';
+                                            else : 
                                                 echo $title;
-                                            echo '</a>';
-                                        else : 
-                                            echo $title;
-                                        endif;
-                                    if ($mediaPresent && have_rows('media')):
-                                        echo '<div class="medias">';
-                                        while (have_rows('media')): the_row();
-                                            $media_item = get_sub_field('media');
-                                            // Check if $media_item is actually an array
-                                            if (is_array($media_item) && isset($media_item['mime_type'], $media_item['url'], $media_item['title'])):
-                                                $media_type = $media_item['mime_type'];
-                                                $image_url = $media_item['url'];
-                                                $media_item_title = $media_item['title'];
-                        
-                                                echo '<div class="media-item">';
-                                                // Check the type and embed accordingly
-                                                if (strpos($media_type, 'image') !== false):
-                                                    // It's an image
-                                                    echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($media_item_title) . '">';
-                                                    echo '<div class="media-title">';
-                                                        echo esc_html($media_item_title);
-                                                    echo '</div>';
-                                                elseif ($media_type == 'application/pdf'):
-                                                    // It's a PDF
-                                                    echo '<iframe src="' . esc_url($image_url) . '" width="100%" height="600" style="border: none;"></iframe>';
-                                                endif;
-                                                echo '</div>';
                                             endif;
-                                        endwhile;
-                                        echo '</div>';
-                                    endif;
-                        
-                                    echo '</li>';
+                                        if ($mediaPresent && have_rows('media')):
+                                            echo '<div class="medias">';
+                                            while (have_rows('media')): the_row();
+                                                $cover_image = get_sub_field('cover_image');
+                                                $media_upload = get_sub_field('media');
+                                                    $image_url = $cover_image['url'];
+                                                    $media_item_title = $cover_image['title'];
+                                                    if ($media_upload) :
+                                                        echo '<a href="' . $media_upload['url'] . '" target="_blank" class="media-item-with-link">'; 
+                                                            echo '<div class="media-item">';
+                                                                echo '<div class="media-item-image">';
+                                                                    echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($media_item_title) . '">';
+                                                                echo '</div>';
+                                                                echo '<div class="media-title">';
+                                                                    echo esc_html($media_item_title);
+                                                                echo '</div>';
+                                                            echo '</div>';
+                                                        echo '</a>';
+                                                    else : 
+                                                        echo '<div class="media-item">';
+                                                            echo '<div class="media-item-image">';
+                                                                echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($media_item_title) . '">';
+                                                            echo '</div>';
+                                                            echo '<div class="media-title">';
+                                                                echo esc_html($media_item_title);
+                                                            echo '</div>';
+                                                        echo '</div>';
+                                                    endif;
+                                            endwhile;
+                                            echo '</div>';
+                                        endif;
+                                        echo '</li>';
                                     echo '</ul>';
                                 endwhile;
                             endif;
